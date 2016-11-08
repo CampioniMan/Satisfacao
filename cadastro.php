7b5304bf64e866562510cdf182b09a2d634202a0
CampioniMan <campioni.daniel@gmail.com>
2016-11-08 18:18:32 -0200

    Primeiro commit

---
+++ cadastro.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+<html>
+	<head>
+		<title>Seja bem-vindo ao Reclame!</title>
+	</head>
+	<body>
+		Cadastro: Deseja reclamar? clique <a href="index.php">aqui</a>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+<html>
+	<head>
+		<title>Seja bem-vindo ao Reclame!</title>
+	</head>
+	<body>
+		Deseja reclamar? clique <a href="cadastro.php">aqui</a>
+	</body>
+</html>
