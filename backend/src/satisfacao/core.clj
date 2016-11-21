06b8f46df779b13e60ec3a0be4edb57354e00a0b
Tiago Dall'Oca <tdalloca@gmail.com>
2016-11-21 12:41:49 -0200

    Métodos padrões feitos

--- backend/src/satisfacao/core.clj
+++ backend/src/satisfacao/core.clj
@@ -7,20 +7,43 @@
 (def date-reg
   #"^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$")
 
-(s/def ::Tipo int?)
-(s/def ::Nome (s/and string? not-empty))
-(s/def ::Mensagem ::Nome)
-(s/def ::Data (s/and string? #(re-matches date-reg %)))
+(s/def ::Tabela (s/and string? not-every?))
+(s/def ::Row s/map-spec)
 
+(s/fdef inserir!
+        :args (s/cat :tabela ::Tabela
+                     :row ::Row))
 
-(s/def ::atendimento (s/keys :req-up [::Tipo ::Nome ::Mensagem ::Data]))
+(s/def ::Where-Vector (s/* (s/cat :select string?
+                                  :value #(not (nil? %)))))
 
-(s/fdef registrar-atendimento
-        :args ::atendimento)
-
-(defn registrar-atendimento
+(s/fdef deletar!
+        :args (s/cat :tabela ::Tabela
+                     :where-vector ::Where-Vector))
+(defn inserir!
   "Registra o map args no bd"
-  [atendimento]
+  [tabela row]
   (j/insert! sqltdb
-             "Atendimento"
-             atendimento))
+             tabela
+             row))
+
+(defn deletar!
+  "Deleta row de acordo com o parâmetro de seleção"
+  [where-vector
+   tabela]
+  (j/delete! sqltdb
+             tabela
+             where-vector))
+
+(defn selecionar!
+  "Seleciona linha(s) da tabela com base no parâmetro de seleção"
+  [where-vector
+   tabela]
+  (let [strings (take-nth 2 where-vector)
+        values (take-nth 2 (rest where-vector))
+        s-string (str "SELECT * FROM `" tabela "` WHERE"
+                      (reduce #(str %1 " " (get %2 0))
+                              ""
+                              strings))] 
+    (j/execute! sqltdb
+                (concat [s-string] values))))
