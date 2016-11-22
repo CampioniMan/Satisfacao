c33a4b501054dfc0bc735d27d1114c5ad90bc01f
Tiago Dall'Oca <tdalloca@gmail.com>
2016-11-22 11:55:01 -0200

    spec's feitos

--- backend/src/satisfacao/core.clj
+++ backend/src/satisfacao/core.clj
@@ -7,16 +7,28 @@
 (def date-reg
   #"^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$")
 
-(s/def ::Tabela (s/and string? not-every?))
+(s/def ::FilledString (s/and string? not-empty))
 (s/def ::Row s/map-spec)
-(s/def ::Where-Vector (s/tuple string? #(not (nil? %))))
+(s/def ::Where-Vector (s/cat :where-string (s/* ::FilledString)
+                             :values (s/* (s/spec #(-> %
+                                                       (nil?)
+                                                       (not))))))
 
 (s/fdef inserir!
-        :args (s/cat :tabela ::Tabela
+        :args (s/cat :tabela ::FilledString
                      :row ::Row))
 
 (s/fdef deletar!
-        :args (s/cat :tabela ::Tabela
+        :args (s/cat :tabela ::FilledString
+                     :where-vector ::Where-Vector))
+
+(s/fdef selecionar!
+        :args (s/cat :tabela ::FilledString
+                     :where-vector ::Where-Vector))
+
+(s/fdef update!
+        :args (s/cat :tabela ::FilledString
+                     :set-map s/map-spec
                      :where-vector ::Where-Vector))
 
 (defn inserir!
